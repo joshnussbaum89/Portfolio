deed19b894a2a8828f3a4cfd7593ac72731653b3
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-01-06 14:05:28 -0600

    update copy, icons, white color

--- js/modules/dots.js
+++ js/modules/dots.js
@@ -22,20 +22,17 @@ function handleActiveNavigationDotStyling(element = this) {
  */
 function handleActiveNavigationDotsOnScroll() {
   // DOM selectors
-  const services = document.getElementById('services')
   const work     = document.getElementById('work')
   const contact  = document.getElementById('contact')
 
   // Section locations
-  const servicesTop = services.getBoundingClientRect().top
   const workTop     = work.getBoundingClientRect().top
   const contactTop  = contact.getBoundingClientRect().top
 
   // Set active dot index
-  if (servicesTop > 0)                  activeDotIndex = 0 // Home
-  if (servicesTop < 100 && workTop > 0) activeDotIndex = 1 // Services
-  if (workTop < 100 && contactTop > 0)  activeDotIndex = 2 // Work
-  if (contactTop < 100)                 activeDotIndex = 3 // Contact
+  if (workTop > 0)                      activeDotIndex = 0 // Home
+  if (workTop < 100 && contactTop > 0)  activeDotIndex = 1 // Work
+  if (contactTop < 100)                 activeDotIndex = 2 // Contact
 
   // Handle styling
   handleActiveNavigationDotStyling(document.querySelector(`.dot[data-index="${activeDotIndex}"]`))
