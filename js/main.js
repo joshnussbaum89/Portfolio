939230d5c56215790195151a92cc1fb25eff087f
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2023-01-10 15:14:08 -0600

    add ai headshot, bouncing arrow, scrolling nav z-index bug, styling

--- js/main.js
+++ js/main.js
@@ -7,23 +7,23 @@ import { toggleVisibleProjects } from './modules/projects.js'
 // Initialize app
 function init() {
   if (!window.location.href.includes('success')) {
-    setFooterDateAndText()
-
     // On Scroll > handle 'active' dots
     document.addEventListener('scroll', handleActiveNavigationDotsOnScroll)
   }
-
   // On Click > toggle 'active' mobile navigation
   hamburger.addEventListener('click', handleMobileNavigationOnClick)
-
+  
   // On Resize > reset 'active' mobile navigation
   window.addEventListener('resize', handleMobileNavigationOnResize)
-
+  
   // On Click > toggle 'active' projects
   projectTags.forEach((tag) => tag.addEventListener('click', toggleVisibleProjects))
-
+  
   // 1. Attach click handler to mobile navigation items
   // 2. On Click > toggle 'active' mobile navigation
   mobileNavItems.forEach((item) => item.addEventListener('click', handleMobileNavigationOnClick))
+
+  // Set footer copywrite info
+  setFooterDateAndText()
 }
 init()
